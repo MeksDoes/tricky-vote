d3e5a812a4c5cedea62a3a4f37eae0d290dc121f
MeksDoes <120422853+MeksDoes@users.noreply.github.com>
2025-04-16 13:49:31 +0200

    Add swagger ui

--- poll-service/src/poll/dto/get-poll.dto.ts
+++ poll-service/src/poll/dto/get-poll.dto.ts
@@ -1,17 +1,22 @@
 import { IsString, MinLength, IsArray } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 import { Option } from '../../options/option.entity';
 
 class PollDto {
+    @ApiProperty()
     @IsString()
     pollId?: string;
 
+    @ApiProperty()
     @IsString()
     @MinLength(2)
     title: string;
 
+    @ApiProperty()
     @IsString()
     question: string;
 
+    @ApiProperty()
     @IsArray()
     options: Array<Option>;
 }
--- poll-service/src/poll/poll.controller.ts
+++ poll-service/src/poll/poll.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import PollDto from './dto/get-poll.dto';
 import { PollService } from 'src/poll/poll.service';
+import { ApiOkResponse } from '@nestjs/swagger';
 
 
 @Controller('polls')
@@ -8,6 +9,7 @@ export class PollController {
   constructor(private readonly pollService: PollService) {}
 
   @Get(':pollId')
+  @ApiOkResponse({type: PollDto})
   getPoll(@Param('pollId') pollId: string): PollDto {
     return this.pollService.getPoll(pollId);
   }
