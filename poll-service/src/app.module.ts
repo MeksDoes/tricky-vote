094e98637f55391dbb1b1a07ef1b37669d1a7319
MeksDoes <120422853+MeksDoes@users.noreply.github.com>
2025-04-16 13:48:51 +0200

    Add getPolls draft

--- poll-service/src/app.module.ts
+++ poll-service/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PollController } from './poll/poll.controller';
+import { PollService } from './poll/poll.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+    PollController
+  ],
+  providers: [
+    AppService,
+    PollService
+  ],
 })
 export class AppModule {}
