99fed492e436d727b9db782467ca4566b9afd5ca
MeksDoes <120422853+MeksDoes@users.noreply.github.com>
2025-04-17 17:01:38 +0200

    Use Mongodb for poll

--- poll-service/src/poll/poll.module.ts
+++ poll-service/src/poll/poll.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { PollController } from './poll.controller';
 import { PollService } from './poll.service';
+import { PollSchema } from './poll.schema';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forFeature([{ name: 'Poll', schema: PollSchema }])],
   controllers: [PollController],
   providers: [PollService],
 })
