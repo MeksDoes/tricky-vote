ade62b6e9c4be0897a87ce13c1b0a1ca2a5942e0
MeksDoes <120422853+MeksDoes@users.noreply.github.com>
2025-04-22 14:38:51 +0200

    Add create poll draft

--- fe-service/src/views/CreatePoll.vue
+++ fe-service/src/views/CreatePoll.vue
@@ -1,9 +1,84 @@
 <template>
-  <h1 v-text="$t('title')" />
-  <h1 v-text="$t('title')" />
-  <h1 v-text="$t('title')" />
-  <h1 v-text="$t('title')" />
-  <h1 v-text="$t('title')" />
+  <div class="create-poll">
+    <BaseInput
+      :label="$t('poll.create.input.title.title')"
+      :placeholder="$t('poll.create.input.title.placeholder')"
+      v-model="title"
+    />
+    <BaseInput
+      :label="$t('poll.create.input.question.title')"
+      :placeholder="$t('poll.create.input.question.placeholder')"
+      v-model="question"
+    />
+
+    <div class="add-option">
+      <BaseInput
+        class="add-option--base-input"
+        :label="$t('poll.create.input.option.title')"
+        :placeholder="$t('poll.create.input.option.placeholder')"
+        v-model="newOption"
+      />
+      <button
+        :disabled="newOption.trim().length === 0"
+        @click="addOption"
+        v-text="$t('poll.create.input.option.addOption')"
+      />
+    </div>
+
+    <div class="create-poll__preview" v-if="options.length > 0">
+      <h3 v-text="$t('poll.create.preview.options')" />
+      <ul>
+        <li v-for="(option, index) in options" :key="index">
+          {{ option }}
+        </li>
+      </ul>
+    </div>
+
+    <button :disabled="pollIsValid" @click="submitPoll" v-text="$t('poll.create.submit')" />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import BaseInput from '@/components/BaseInput.vue';
+
+const title = ref('');
+const question = ref('');
+const newOption = ref('');
+const options = ref<string[]>([]);
+
+const pollIsValid = computed(() => {
+  return true;
+});
+
+function addOption() {
+  if (newOption.value.trim().length === 0) return;
+  options.value.push(newOption.value.trim());
+  newOption.value = '';
+}
+
+function submitPoll() {
+  console.log('submitPoll');
+}
+</script>
+
+<style scoped>
+.add-option {
+  margin-top: var(--margin-m);
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+
+.add-option button {
+  align-self: flex-end;
+}
+
+.add-option--base-input {
+  width: 100%;
+}
+
+.create-poll__preview {
+  margin-top: var(--margin-l);
+}
+</style>
