71895710110ec62dabb6f9f21e27676c347072f3
MeksDoes <120422853+MeksDoes@users.noreply.github.com>
2025-04-22 21:38:19 +0200

    Show poll requirements in swagger

--- poll-service/src/poll/dto/create-poll.dto.ts
+++ poll-service/src/poll/dto/create-poll.dto.ts
@@ -12,17 +12,30 @@ import { Type } from 'class-transformer';
 import { OptionDto } from '../../options/dto/option.dto';
 
 export class CreatePollDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'The title of the poll',
+    minLength: 2,
+    example: 'Favorite food',
+  })
   @IsString()
   @MinLength(2)
   title: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'The question of the poll',
+    minLength: 5,
+    example: 'What is your favorite food?',
+  })
   @IsString()
   @MinLength(5)
   question: string;
 
-  @ApiProperty({ type: [OptionDto] })
+  @ApiProperty({
+    description: 'The options for the poll',
+    type: [OptionDto],
+    minItems: 2,
+    example: [{ text: 'Pizza' }, { text: 'Lasange' }],
+  })
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => OptionDto)
