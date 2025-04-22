c04791867f72f97abae1323bc54f04b97e032e51
MeksDoes <120422853+MeksDoes@users.noreply.github.com>
2025-04-22 11:23:59 +0200

    Setup i18n

--- fe-service/src/main.ts
+++ fe-service/src/main.ts
@@ -1,11 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+
+import './assets/main.css'
+import { i18n } from './i18n/i18n'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(i18n)
 
 app.mount('#app')
