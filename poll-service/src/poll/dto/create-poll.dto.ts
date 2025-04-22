9b40f345f597df90596cce65042460a524654477
MeksDoes <120422853+MeksDoes@users.noreply.github.com>
2025-04-22 20:47:32 +0200

    Use pollId as identifier

--- poll-service/src/poll/dto/create-poll.dto.ts
+++ poll-service/src/poll/dto/create-poll.dto.ts
@@ -1,10 +1,4 @@
-import {
-  IsString,
-  MinLength,
-  IsArray,
-  ValidateNested,
-  ArrayUnique,
-} from 'class-validator';
+import { IsString, MinLength, IsArray, ValidateNested, ArrayUnique } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 
