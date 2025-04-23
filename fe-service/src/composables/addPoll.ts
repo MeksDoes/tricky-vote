9b0e70c1bddcf3e2edfc21c0bfaa7c42e56e229f
MeksDoes <120422853+MeksDoes@users.noreply.github.com>
2025-04-23 11:58:26 +0200

    Add create poll

---
+++ fe-service/src/composables/addPoll.ts
@@ -0,0 +1,52 @@
+import { ref } from 'vue';
+import { AxiosError } from 'axios';
+
+import { API } from '../api';
+import { type APIResponse } from '../api/types';
+
+import { type CreatePollDTO, type Poll } from '../api/polls/types';
+
+export function addPoll() {
+  const poll = ref<Poll>();
+  const isLoading = ref(false);
+  const error = ref<AxiosError<string> | null>(null);
+
+  async function dispatchCreatePoll(input: CreatePollDTO): Promise<APIResponse<null>> {
+    isLoading.value = true;
+
+    try {
+      const { status, data } = await API.polls.createPoll(input);
+
+      if (status === 201) {
+        poll.value = data.content;
+
+        return {
+          success: true,
+          content: null,
+        };
+      }
+    } catch (err) {
+      error.value = err as AxiosError<string>;
+      return {
+        success: false,
+        status: error.value.response?.status,
+        content: null,
+      };
+    } finally {
+      isLoading.value = false;
+    }
+
+    return {
+      success: false,
+      content: null,
+      status: 400,
+    };
+  }
+
+  return {
+    postPoll: dispatchCreatePoll,
+    poll,
+    isLoading,
+    error,
+  };
+}
