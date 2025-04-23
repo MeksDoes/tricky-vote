0c9a472b12a40576c503e424f751f55651025a9b
MeksDoes <120422853+MeksDoes@users.noreply.github.com>
2025-04-23 13:15:23 +0200

    Add show poll

---
+++ fe-service/src/composables/getPoll.ts
@@ -0,0 +1,46 @@
+import { ref } from 'vue';
+import { AxiosError } from 'axios';
+
+import { API } from '../api';
+import { type APIResponse } from '../api/types';
+
+export function fetchPoll() {
+  const isLoading = ref(false);
+  const error = ref<AxiosError<string> | null>(null);
+
+  async function dispatchGetPollById(pollId: string): Promise<APIResponse<null>> {
+    isLoading.value = true;
+
+    try {
+      const { status, data } = await API.polls.getPoll(pollId);
+
+      if (status === 200) {
+        return {
+          success: true,
+          content: data,
+        };
+      }
+    } catch (err) {
+      error.value = err as AxiosError<string>;
+      return {
+        success: false,
+        status: error.value.response?.status,
+        content: null,
+      };
+    } finally {
+      isLoading.value = false;
+    }
+
+    return {
+      success: false,
+      content: null,
+      status: 400,
+    };
+  }
+
+  return {
+    getPoll: dispatchGetPollById,
+    isLoading,
+    error,
+  };
+}
---
+++ fe-service/src/views/ShowPoll.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="poll-view">
+    <div v-if="isLoading">{{ $t('common.loading.message') }}</div>
+    <div v-else-if="error">{{ error }}</div>
+    <div v-else-if="poll">
+      <h1 class="poll-view__title">{{ poll.title }}</h1>
+      <h2 class="poll-view__question">{{ poll.question }}</h2>
+
+      <div class="poll-view__options">
+        <h3>{{ $t('poll.option.title') }}</h3>
+        <ul>
+          <li v-for="(option, index) in poll.options" :key="index">
+            {{ option.text }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+
+import { type Poll } from '../api/polls/types';
+import { fetchPoll } from '../composables/getPoll';
+
+const { pollId } = useRoute().params as { pollId: string };
+const { getPoll, isLoading, error } = fetchPoll();
+const poll = ref<Poll>();
+
+onMounted(async () => {
+  const { content, success, status } = await getPoll(pollId);
+  if (!success) {
+    console.error('Error fetching poll:', status);
+    return;
+  }
+  poll.value = content;
+});
+</script>
+
+<style scoped>
+.poll-view {
+  padding-top: var(--padding-l);
+}
+
+.poll-view__title {
+  margin-bottom: var(--margin-l);
+}
+
+.poll-view__question {
+  margin-bottom: var(--margin-m);
+}
+
+.poll-view__options {
+  margin-top: var(--margin-m);
+}
+
+.poll-view__options h3::after {
+  content: ':';
+}
+
+.poll-view__options ul {
+  margin-top: var(--margin-s);
+}
+
+.poll-view__options li {
+  background-color: var(--color-gray-light);
+  margin-bottom: var(--margin-s);
+  padding: var(--padding-s);
+  border-radius: var(--radius-s);
+}
+</style>
