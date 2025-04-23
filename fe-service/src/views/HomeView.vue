1d8582b91143b2f7818155787646802f2dad1142
MeksDoes <120422853+MeksDoes@users.noreply.github.com>
2025-04-23 11:58:40 +0200

    Show existing polls

--- fe-service/src/views/HomeView.vue
+++ fe-service/src/views/HomeView.vue
@@ -2,17 +2,32 @@
   <h1 v-text="$t('title')" />
 
   <button @click="goToCreatePoll" v-text="$t('poll.create.newPoll')" />
+
+  <div class="polls">
+    <div v-for="(poll, index) in polls" :key="index" class="polls__item">
+      <h2 v-text="poll.title" />
+      <p v-text="poll.pollId" />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import { useRouter } from 'vue-router';
-import { useFetch } from '../composables/getPolls';
 
-// const { getPolls, polls, isLoading, error } = useFetch();
-// await getPolls();
-// console.log(polls);
-// console.log(isLoading);
-// console.log(error);
+import { fetchPolls } from '../composables/getPolls';
+const { getPolls, polls, isLoading, error } = fetchPolls();
+
+onMounted(async () => {
+  const { success, status } = await getPolls();
+  console.log({ polls });
+  console.log(isLoading);
+  console.log(error);
+
+  if (!success) {
+    console.log('Api status ->', status);
+  }
+});
 
 // debugger;
 
@@ -22,3 +37,13 @@ const goToCreatePoll = (): void => {
   router.push('/create-poll');
 };
 </script>
+
+<style scoped>
+.polls {
+  padding-top: var(--padding-l);
+}
+
+.polls__item {
+  padding-top: var(--padding-m);
+}
+</style>
