e71e1fd2bef16da10d7f388400716f190a65d48d
MeksDoes <120422853+MeksDoes@users.noreply.github.com>
2025-04-28 17:52:57 +0200

    Add router spec

--- fe-service/src/router/index.ts
+++ fe-service/src/router/index.ts
@@ -2,25 +2,27 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import CreatePoll from '../views/CreatePoll.vue';
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/create-poll',
+    name: 'create-poll',
+    component: CreatePoll,
+  },
+  {
+    path: '/poll/:pollId',
+    name: 'show-poll',
+    component: () => import('@/views/ShowPoll.vue'),
+  },
+];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/create-poll',
-      name: 'create-poll',
-      component: CreatePoll,
-    },
-    {
-      path: '/poll/:pollId',
-      name: 'show-poll',
-      component: () => import('@/views/ShowPoll.vue'),
-    },
-  ],
+  routes,
 });
 
 export default router;
